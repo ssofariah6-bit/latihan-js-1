17fd8fd045208ef357ef3a3890b2278c76abd3e4
ssofariah6-bit <ssofariah6@gmail.com>
2025-09-11 09:01:29 +0700

    file latihan while

---
+++ lat-hari2.js
@@ -0,0 +1,25 @@
+var nilai= 0;
+while (nilai>=0 && nilai<=14){
+    console.log(nilai);
+    nilai+=2;
+}
+
+var nilai2=1;
+while (nilai2>=1 && nilai2 <=15){
+    console.log(nilai2);
+    nilai2+=2;
+}
+
+
+
+/*angka = 14;
+if (angka %2 ===0 && nilai<=14){
+    console.log(angka);
+}*/
+
+
+/*for (angka=14; angka%2===0; angka--){
+    console.log(angka);
+}*/
+
+
